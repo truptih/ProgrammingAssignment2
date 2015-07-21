733f0b2ca2d665dd7ec9a2d3717e48c531867e11
truptih <trupti_harsora@hotmail.com>
2015-07-21 16:13:32 +0530

    commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+## make invert matrix value initally null
+  invert_matrix<-NULL
+## set the value of matrix
+  set<-function(y){
+    x<<-y
+    invert_matrix<<-NULL
+  }
+## get the value of matrix
+  get<-function(){
+    x
+  }
+## get the inverse value of matrix
+  get_invert_matrix<-function() {
+    invert_matrix
+  }
+## set the inverse value of matrix
+  set_invert_matrix<-function(i_m_val){
+    invert_matrix<<- i_m_val
+  }
+## list the method
+ list(get=get,set=set,
+      get_invert_matrix=get_invert_matrix,
+      set_invert_matrix=set_invert_matrix)
 
+  
 }
 
 
@@ -12,4 +36,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ##get the inverse if it is alreay there in cache
+   invert_matrix<-x$get_invert_matrix()
+   if(!is.null(invert_matrix)){
+     message("getting cache data")
+     return(invert_matrix)
+   }
+  ## else get the matrix value and inverse the matri by using solve() method
+   data<-x$get()
+   invert_matrix<-solve(data, ...)
+   x$set_invert_matrix(invert_matrix)
+   invert_matrix
+   
 }
